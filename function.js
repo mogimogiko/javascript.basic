46bfb0433f236723e4e71258f73c67d381a04b65
mogimogiko <moemmmoe66@gmail.com>
2024-03-30 14:12:57 +0900

    関数を追加

---
+++ function.js
@@ -0,0 +1,17 @@
+// 朝の挨拶を出力する関数を定義する
+const sayGoodMorning = () => {
+  console.log('おはようございます！');
+  console.log('昨日はよく眠れましたか？');
+  console.log('今日も一日頑張りましょう！');
+}
+
+// 夜の挨拶を出力する関数を定義する
+const sayGoodEvening = () => {
+  console.log('こんばんは！');
+  console.log('今日も一日お疲れさまでした。');
+}
+
+// 朝の挨拶を出力する関数を呼び出す
+sayGoodMorning();
+// 夜の挨拶を出力する関数を呼び出す
+sayGoodEvening();
