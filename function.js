a79d8f93080fea86e2be60e88ff8a8122ef062df
mogimogiko <moemmmoe66@gmail.com>
2024-04-01 03:09:01 +0900

    引数と戻り値を追加

--- function.js
+++ function.js
@@ -15,3 +15,26 @@ const sayGoodEvening = () => {
 sayGoodMorning();
 // 夜の挨拶を出力する関数を呼び出す
 sayGoodEvening();
+
+
+// 与えられた引数priceに送料を加算し、その値を出力する関数を定義する 
+const calculateTotal = (price) => {
+  console.log(price + 500 + '円');
+}
+// 関数を呼び出し、引数として購入金額を渡す
+calculateTotal(1200);
+
+// 与えられた引数priceと引数shippingFeeを加算し、その値を出力する関数を定義する
+const addTwoArguments = (price, shippingFee) => {
+  console.log(price + shippingFee + '円');
+}
+// 関数を呼び出し、引数として購入金額と送料を渡す
+addTwoArguments(1200, 500);
+
+
+// 与えられた引数numを２倍にし、その値を戻り値として返す関数を定義する
+const double = (num) => {
+  return num * 2;
+}
+// 関数の戻り値を出力する
+console.log(double(30));
